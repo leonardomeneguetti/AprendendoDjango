cf01c89b4f89e8897f1ed46f36625a262c27c45b
Leonardo Meneguetti <leonardo.meneguetti@aluno.ifsp.edu.br>
2024-06-26 16:22:07 -0300

    Aula7 - Aula12

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
+from django.urls import reverse
+from .models import Topic
+from .forms import TopicForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def index(request):
-    """Página principal do Learning_Log"""
+    """Página principal do Learning_Log."""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Mostra todos os assuntos."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Mostra um único assunto e todas as suas entradas."""
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Adiciona um novo assunto."""
+    if request.method != 'POST':
+        #Nenhum dado submetido; cria um formulário em branco
+        form = TopicForm()
+    else:
+        #Dados de POST submetido; processa os dados
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topics'))
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
